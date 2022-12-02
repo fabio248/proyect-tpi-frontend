ae6ab41be32cc9cac9f9b8a619f09e5ee64b3d44
ML19017 <71608019+ML19017@users.noreply.github.com>
2022-12-01 22:02:54 -0600

    Update Clientes.jsx
    Se añade modal pedido y controles; se incorpora manejo de excepciones para las peticiones.

--- src/pages/Dashboard/Clientes.jsx
+++ src/pages/Dashboard/Clientes.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { CircularProgress, Box, IconButton, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip } from "@mui/material";
-import UpdateIcon from "@mui/icons-material/Update";
-import DeleteIcon from "@mui/icons-material/Delete";
-import AddIcon from "@mui/icons-material/Add";
+import { CircularProgress, Button, Box, IconButton, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip } from "@mui/material";
+import { Update, Delete, Add } from "@mui/icons-material";
+import { Dialog, DialogContent, DialogContentText, DialogTitle, DialogActions} from "@mui/material";
+import AlertError from '../../components/AlertError';
 
 class Clientes extends Component {
 
@@ -10,29 +10,42 @@ class Clientes extends Component {
         super(props);
 
         this.state = {
-            clientes: {},
+            error:false,
+            mensajeError:"",
+            clientes:{},
             nulo:true,
+            estadoModalPedido:false,
+            clienteSeleccionado:{},
         };
     }
 
     // Obtiene los clientes
     async getClientes() {
-        
-        const peticion = await fetch("https://proyecto-tpi.onrender.com/api/v1/clients", {
+        fetch("https://proyecto-tpi.onrender.com/api/v1/clients", {
             method:"GET",
             headers: {
+                "Content-Type": "application/json",
                 "API":"78b96cea5c47cf11ae257dd16dd09e809f5bb205c29db1fdde1a33bede7e873b",
                 "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhMmJlN2MwZS01Mjc1LTQyNjEtYmZiMC1jMDcxYWE4NGI1NTIiLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE2NjkzNDAzNDd9.yh76A2ekWXODxuAIdsRmdtB9KOr4kdFGtULH9QWlQR8",
             }
-        });
-        
-        peticion.json().then(data => {
-            this.setState({
-                clientes:data,
-                nulo:false,
+        }).then(async response => {
+            const data = await response.json();
+            // Verifica el estado de la respuesta
+            if (!response.ok) {
+                // Obtiene el error de la respuesta y rechaza la promesa
+                const error = (data && data.message) || response.statusText;
+                return Promise.reject(error);
+            }
+            this.setState({ 
+                clientes: data,
+                nulo: false,
             })
-        }).catch(exp => {
-            console.log(exp)
+        })
+        .catch(error => {
+            this.setState({ 
+                errorMessage: error.toString(),
+                error: true, 
+            });
         });
     }
 
@@ -48,6 +61,59 @@ class Clientes extends Component {
         
     }
 
+    updateTable() {
+        this.getClientes();
+    }
+
+    /* ----------------------------------------------------------- *
+        BLOQUE DE CODIGO: AGREGAR PEDIDO.
+        NOTA: Incluye controles del formulario modal.
+    * ------------------------------------------------------------ */
+    async agregarPedido() {
+        fetch("https://proyecto-tpi.onrender.com/api/v1/clients", {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+                "API":"78b96cea5c47cf11ae257dd16dd09e809f5bb205c29db1fdde1a33bede7e873b",
+                "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhMmJlN2MwZS01Mjc1LTQyNjEtYmZiMC1jMDcxYWE4NGI1NTIiLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE2NjkzNDAzNDd9.yh76A2ekWXODxuAIdsRmdtB9KOr4kdFGtULH9QWlQR8",
+            }
+        }).then(async response => {
+            const data = await response.json();
+            // Verifica el estado de la respuesta
+            if (!response.ok) {
+                // Obtiene el error de la respuesta y rechaza la promesa
+                const error = (data && data.message) || response.statusText;
+                return Promise.reject(error);
+            }
+            this.setState({ 
+                clientes: data,
+                nulo: false,
+            })
+        })
+        .catch(error => {
+            this.setState({ 
+                errorMessage: error.toString(),
+                error: true, 
+            });
+        });
+    }
+
+    abrirAgregarPedido(cliente) {
+        this.setState({
+            estadoModalPedido:true,
+            clienteSeleccionado:cliente,
+        });
+    }
+
+    cerrarAgregarPedido() {
+        this.setState({
+            estadoModalPedido:false,
+            clienteSeleccionado:undefined,
+        });
+    }
+    /* ----------------------------------------------------------- *
+        FIN DEL BLOQUE DE CODIGO
+    * ------------------------------------------------------------ */
     render() {
 
         if(this.state.nulo) {
@@ -62,7 +128,7 @@ class Clientes extends Component {
                         direction="column"
                         alignItems="center"
                         justifyContent="center"
-                        style={{ minHeight: '100vh' }}
+                        style={{ minHeight: "100vh" }}
                     >
                         <Box>
                             <CircularProgress color={"primary"} />
@@ -77,11 +143,19 @@ class Clientes extends Component {
         else {
             return(
                 <React.Fragment>
+                    {this.state.error && (
+                        <AlertError message={"Se ha producido un error! CODE:" + this.state.mensajeError} />
+                    )}
                     <div className="container m-2 p-3">
-                        <div className='text-center'>
-                            <h3 className='section-subheading text-muted'>
+                        <div className="text-center">
+                            <h3 className="section-subheading text-muted">
                                 Tabla de Clientes Registrados
                             </h3>
+                        </div>
+                        <div className="container m-1 p-1">
+                            <Button variant="outlined" color="success">
+                                Agregar Cliente
+                            </Button>
                         </div>
                     </div>
                     <TableContainer>
@@ -134,9 +208,9 @@ class Clientes extends Component {
                                                             <IconButton
                                                                 size={"small"}
                                                                 color={"success"}
-                                                                onClick={this.add()}
+                                                                onClick={() => this.abrirAgregarPedido(cliente)}
                                                             >
-                                                                <AddIcon/>
+                                                                <Add />
                                                             </IconButton>
                                                         </Tooltip>
                                                         <Tooltip title={"Actualizar Cliente"}>
@@ -145,7 +219,7 @@ class Clientes extends Component {
                                                                 color={"warning"}
                                                                 onClick={this.update()}
                                                             >
-                                                                <UpdateIcon />
+                                                                <Update />
                                                             </IconButton>
                                                         </Tooltip>
                                                         <Tooltip title="Borrar Cliente">
@@ -154,7 +228,7 @@ class Clientes extends Component {
                                                                 color={"error"}
                                                                 onClick={this.delete()}
                                                             >
-                                                                <DeleteIcon />
+                                                                <Delete />
                                                             </IconButton>
                                                         </Tooltip>
                                                     </Box>
@@ -166,6 +240,17 @@ class Clientes extends Component {
                             </TableBody>
                         </Table>
                     </TableContainer>
+
+                    <Dialog open={this.state.estadoModalPedido} onClose={() => this.cerrarAgregarPedido()} aria-describedby="">
+                        <DialogTitle>Formulario de Pedido</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText id="" color="black">ZzZz</DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={() => this.cerrarAgregarPedido()}>Cancelar</Button>
+                            <Button variant="outlined" color={"error"} onClick={() => this.agregarPedido()}>Eliminar</Button>
+                        </DialogActions>
+                    </Dialog>
                 </React.Fragment>
             )
         }
