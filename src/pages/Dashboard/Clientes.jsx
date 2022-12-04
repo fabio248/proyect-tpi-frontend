c0bfb3e02a5af1bb0038d4137c9fb2475aef1752
Alexandr-ml <68412919+Alexandr-ml@users.noreply.github.com>
2022-12-03 21:22:13 -0600

    corregido headers

--- src/pages/Dashboard/Clientes.jsx
+++ src/pages/Dashboard/Clientes.jsx
@@ -95,19 +95,20 @@ class Clientes extends Component {
     * ------------------------------------------------------------ */
     async agregarPedido() {
 
+        const cuerpo = {
+            clienteId:  this.state.clienteSeleccionado.id,
+            fechaEntrega: new Date(this.state.fecha).toISOString(),
+            type: this.state.pedido
+        }
+
         if(!(this.state.pedido === "") && dayjs(this.state.fecha).isValid()) {
             fetch("https://proyecto-tpi-backend-production.up.railway.app/api/v1/tasks", {
                 method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "API":"78b96cea5c47cf11ae257dd16dd09e809f5bb205c29db1fdde1a33bede7e873b",
-                    "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhMmJlN2MwZS01Mjc1LTQyNjEtYmZiMC1jMDcxYWE4NGI1NTIiLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE2NjkzNDAzNDd9.yh76A2ekWXODxuAIdsRmdtB9KOr4kdFGtULH9QWlQR8",
+                headers:{
+                    "api":"78b96cea5c47cf11ae257dd16dd09e809f5bb205c29db1fdde1a33bede7e873b",
+                    "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhMmJlN2MwZS01Mjc1LTQyNjEtYmZiMC1jMDcxYWE4NGI1NTIiLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE2NjkzNDAzNDd9.yh76A2ekWXODxuAIdsRmdtB9KOr4kdFGtULH9QWlQR8"
                 },
-                body: {
-                    "clienteId":  this.state.clienteSeleccionado.id,
-                    "fechaEntrega": new Date(this.state.fecha).toISOString(),
-                    "type": this.state.pedido,
-                }
+                body: JSON.stringify(cuerpo)
             }).then(async response => {
                 const data = await response.json();
                 // Verifica el estado de la respuesta
