aa4ccae433002d1c91b416b7721eda97d503b2ae
Daniel Serrano <sz19002@ues.edu.sv>
2022-12-05 20:12:17 -0600

    pantalla ActualizarPedido funcional y con estilos

--- src/pages/Dashboard/ActualizarPedido.jsx
+++ src/pages/Dashboard/ActualizarPedido.jsx
@@ -50,16 +50,18 @@ function enviarCambios(newPedido) {
   const body = {
     type: newPedido.type,
     fechaEntrega: new Date(newPedido.fechaEntrega).toISOString(),
-  }
+  };
   console.log(body);
 
-  axios.patch(url, body,params).then((response) => {
-    if(response.status == 200){
-      alert("El pedido se ha actualizado correctamente")
-      location.href = "/dashboard-admin/pedidos";
-    } else alert("Ocurrió un error")
-  }).catch((er) => console.log(er))
-
+  axios
+    .patch(url, body, params)
+    .then((response) => {
+      if (response.status == 200) {
+        alert("El pedido se ha actualizado correctamente");
+        location.href = "/dashboard-admin/pedidos";
+      } else alert("Ocurrió un error");
+    })
+    .catch((er) => console.log(er));
 }
 
 function ActualizarPedido() {
@@ -77,6 +79,7 @@ function ActualizarPedido() {
     if (pedido.cliente.gender === "Femenino") {
       select = (
         <select
+          className="form-control custom-select"
           onClick={(e) => (newPedido.type = e.target.value)}
           id="selectTipo"
         >
@@ -100,6 +103,7 @@ function ActualizarPedido() {
     } else {
       select = (
         <select
+          className=" form-control custom-select"
           onClick={(e) => (newPedido.type = e.target.value)}
           id="selectTipo"
         >
@@ -130,84 +134,109 @@ function ActualizarPedido() {
 
     x = (
       <>
-        <h3>Datos del cliente</h3>
-        <p>
-          <strong>ID:</strong> {pedido.cliente.id}
-        </p>
-        <p>
-          <strong>Nombres:</strong> {pedido.cliente.firstName}{" "}
-          {pedido.cliente.secondName}
-        </p>
-        <p>
-          <strong>Apellidos:</strong> {pedido.cliente.firstLastName}{" "}
-          {pedido.cliente.secondLastName}
-        </p>
-        <p>
-          <strong>Fecha entrega: </strong> {pedido.fechaEntrega}
-        </p>
-        <p>
-          <strong>Género:</strong> {pedido.cliente.gender}
-        </p>
+        <h4 className="text-center text-muted">Datos del cliente</h4>
+        <div className="p-1">
+          <p>
+            <strong>ID:</strong> {pedido.cliente.id}
+          </p>
+          <p>
+            <strong>Nombres:</strong> {pedido.cliente.firstName}{" "}
+            {pedido.cliente.secondName}
+          </p>
+          <p>
+            <strong>Apellidos:</strong> {pedido.cliente.firstLastName}{" "}
+            {pedido.cliente.secondLastName}
+          </p>
+          <p>
+            <strong>Fecha entrega: </strong>{" "}
+            {pedido.fechaEntrega.substring(0, 10)}
+          </p>
+          <p>
+            <strong>Género:</strong> {pedido.cliente.gender}
+          </p>
+        </div>
+        <h4 className="text-center text-muted mb-3">Opciones del pedido</h4>
 
-        <h3>Opciones del pedido</h3>
+        <div>
+          <form>
+            <div
+              className="d-flex flex-col justify-content-around"
+              style={{ gap: "20%", margin: "0 auto" }}
+            >
+              <div>
+                <label className="mb-2 d-block text-center">
+                  Tipo de prenda
+                </label>
+                {select}
+              </div>
 
-        <form>
-          <label>Tipo de prenda</label>
-          {select}
-          <label>Fecha de entrega</label>
-          <input
-            onChange={(e) => {
-              // console.log(e.target.defaultValue);
-              newPedido.fechaEntrega = e.target.value;
-              console.log(newPedido);
-            }}
-            type={"datetime-local"}
-            defaultValue={pedido.fechaEntrega.substring(0,16)}
-          ></input>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              location.href = "/dashboard-admin/pedidos";
-            }}
-          >
-            Cancelar
-          </button>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              enviarCambios(newPedido);
-            }}
-          >
-            Enviar
-          </button>
-        </form>
+              <div>
+                <label className="mb-2 d-block text-center">
+                  Fecha de entrega
+                </label>
+                <input
+                  className="form-control"
+                  onChange={(e) => {
+                    // console.log(e.target.defaultValue);
+                    newPedido.fechaEntrega = e.target.value;
+                    console.log(newPedido);
+                  }}
+                  type={"datetime-local"}
+                  defaultValue={pedido.fechaEntrega.substring(0, 16)}
+                ></input>
+              </div>
+            </div>
+
+            <div className="mt-5 d-flex flex-col">
+              <div className="" style={{margin: "0 auto"}}>
+              <button className="mx-3 btn btn-danger"
+                onClick={(e) => {
+                  e.preventDefault();
+                  location.href = "/dashboard-admin/pedidos";
+                }}
+              >
+                Cancelar
+              </button>
+              <button className="mx-5 btn btn-success"
+                onClick={(e) => {
+                  e.preventDefault();
+                  enviarCambios(newPedido);
+                }}
+              >
+                Enviar
+              </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </>
     );
   } else
     x = (
-      <>
-        <CircularProgress color={"primary"} />
-        <h6 color="primary">Obteniendo Datos...</h6>
-      </>
+      <div
+        className="card p-5 p-3 mb-5 rounded"
+        style={{ width: "70%", margin: "5rem auto" }}
+      >
+        <CircularProgress
+          className="mb-5"
+          style={{ margin: "0 auto" }}
+          color={"primary"}
+        />
+        <h6 style={{ margin: "0 auto" }} color="primary">
+          Obteniendo Datos...
+        </h6>
+      </div>
     );
 
   return (
-    <>
-      <h1>Actualizar pedido</h1>
+    <div
+      className="card p-5 shadow-lg p-3 mb-5 bg-white rounded"
+      style={{ width: "70%", margin: "5rem auto" }}
+    >
+      <h1 className="mb-5 text-center">Actualizar pedido</h1>
       {x}
-    </>
+    </div>
   );
-}
-
-{
-  /* <Route 
-          path='/dashboard-admin/clientes/:idCliente'
-          element={
-            <Dashboard>
-              <Actualizar />
-            </Dashboard>
-          }
-        ></Route> */
 }
 
 export default ActualizarPedido;
